a2070c247fc5e1ff88b9930fe2d5c7439096918c
REACT-DEVELOPER-SHOHIJAHON <shohijahongulomov32@gmail.com>
2024-09-23 10:39:04 +0500

    finish

--- src/routes/dashboard/profile/Profile.jsx
+++ src/routes/dashboard/profile/Profile.jsx
@@ -15,10 +15,10 @@ const Profile = () => {
     <div className="w-full flex">
     <div className='user flex w-full justify-center items-center'>
       {user ? (
-        <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8 text-center">
+        <div className="max-w-md w-full bg-white shadow-lg p-8 text-center">
           <img
             src={user?.avatar || 'https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small_2x/user-profile-icon-free-vector.jpg'}
-            className="w-48 h-48 rounded-full mx-auto mb-6 border-4 border-purple-600 object-cover shadow-lg"
+            className="w-48 h-48 mx-auto mb-6  object-cover shadow-lg"
             alt={`${user.first_name} ${user.last_name}`}
           />
           <h1 className="text-4xl font-bold text-gray-800 mb-2">
